a7acdf7c1026881d4f6c128b2b6954b6afc8b813
HaZ3R0 <megamackan@outlook.com>
2023-11-22 10:22:40 +0100

    Add project files.

---
+++ uppgift5.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace uppgift5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            Console.WriteLine("Skriv in 3 tal med mellanrum i mellan");
+            string talen = Console.ReadLine();
+            string[] x = talen.Split(' ');
+            int a = int.Parse(x[0]);
+            int b = int.Parse(x[1]);
+            int c = int.Parse(x[2]);
+
+            int sum = (a + b + c);
+            Console.WriteLine(sum);
+        }
+    }
+}
